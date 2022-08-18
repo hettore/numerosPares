2c10f15b38ac231b2caa091925d68406a5f75a90
Hettore Eduardo <duh.barretoo@gmail.com>
2022-08-18 11:54:45 -0300

    Exercício sobre vetores, números pares.

---
+++ src/application/Program.java
@@ -0,0 +1,52 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Quantos números você vai digitar? ");
+		int n = sc.nextInt();
+		
+		int vetor[] = new int[n];
+		
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.print("Digite um número: ");
+			vetor[i] = sc.nextInt();
+			
+		}
+		
+		System.out.print("Valores: ");
+		
+		for (int i = 0; i < vetor.length; i++) {
+			System.out.printf("%d ", vetor[i]);
+		}
+		
+		System.out.println();
+		System.out.print("Números pares digitados: ");
+		//int conta = 0;
+		for (int i = 0; i < vetor.length; i++) {
+			if (vetor[i] % 2 == 0) {
+				System.out.printf("%d ", vetor[i]);
+			}
+		}
+		System.out.println();
+		System.out.print("Quantidade de números pares: ");
+		
+		int contar = 0;
+		for (int i = 0; i < vetor.length; i++) {
+			if(vetor[i] % 2 == 0) {
+				contar++; 
+				
+			}
+			
+		}
+		System.out.printf("%d ", contar);
+		
+		
+	}
+
+}
